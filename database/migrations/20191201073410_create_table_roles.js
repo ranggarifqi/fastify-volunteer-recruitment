39599cfa0072e2474951f419d2da8a0e55817cc1
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-12-01 08:10:05 +0700

    Setup migration for roles, users, user_profiles

--- database/migrations/20191201073410_create_table_roles.js
+++ database/migrations/20191201073410_create_table_roles.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("roles", table => {
+    table.increments("id");
+    table.string("name");
+    table.string("description");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("roles");
 };
---
+++ database/migrations/20191201080404_create_table_user_profiles.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("user_profiles", t => {
+    t.increments("id");
+    t.integer("user_id").unsigned();
+    t.string("full_name");
+    t.string("mobile");
+    t.string("gender", 1);
+    t.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("user_profiles");
+};
