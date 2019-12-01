2bc634cb1c89f7f5e29a75528b98936b207d1b99
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-12-01 14:42:52 +0700

    authenticate decorator

--- plugins/jwt.js
+++ plugins/jwt.js
@@ -6,4 +6,12 @@ module.exports = fp(async function(fastify, opts) {
   fastify.register(require('fastify-jwt'), {
     secret: process.env.JWT_SECRET
   });
+
+  fastify.decorate("authenticate", async (req, reply) => {
+    try {
+      await req.jwtVerify();
+    } catch (error) {
+      reply.send(err);
+    }
+  });
 });
