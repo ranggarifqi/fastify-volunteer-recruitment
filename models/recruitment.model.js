4e01a27539cf156d63a6483e929c1960eeb92866
Rangga Rifqi Pratama <rangga.rifqi.pratama@gmail.com>
2019-12-02 21:19:44 +0700

    add model recruitment

--- models/recruitment.model.js
+++ models/recruitment.model.js
@@ -9,14 +9,19 @@ class Recruitment extends Model {
 
   static get jsonSchema() {
     return {
-      // type: "object",
-      // required: ["name"],
-      // properties: {
-      //   id: { type: "integer" },
-      //   name: { type: "string" },
-      //   description: { type: "string" },
-      //   created_at: { type: "string" }
-      // }
+      type: "object",
+      required: ["title", "sub_title", "description", "submit_description", "start_date", "end_date"],
+      properties: {
+        id: { type: "integer" },
+        title: { type: "string" },
+        sub_title: { type: "string" },
+        description: { type: "string" },
+        submit_description: { type: "string" },
+        created_at: { type: "string" },
+        start_date: { type: "string" },
+        end_date: { type: "string" },
+        is_active: { type: "boolean" },
+      }
     };
   }
 
